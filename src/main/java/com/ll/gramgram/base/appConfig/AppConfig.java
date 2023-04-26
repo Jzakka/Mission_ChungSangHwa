6852c2892ec9c4255370fe57af8d97f3643fefc7
Jzakka <mouse4786@gmail.com>
2023-04-26 19:46:22 +0900

    feat: 수정&삭제 쿨타임

--- src/main/java/com/ll/gramgram/base/appConfig/AppConfig.java
+++ src/main/java/com/ll/gramgram/base/appConfig/AppConfig.java
@@ -3,9 +3,10 @@ package com.ll.gramgram.base.appConfig;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
     @Getter
     private static long likeablePersonFromMax;
 
