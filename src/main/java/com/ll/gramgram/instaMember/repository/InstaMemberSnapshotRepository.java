1db21c7564adad5d83a53e3149eda1676f04c9f4
Jzakka <mouse4786@gmail.com>
2023-05-02 22:15:52 +0900

    main브랜치 동기화 중

--- src/main/java/com/ll/gramgram/instaMember/repository/InstaMemberSnapshotRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ll.gramgram.instaMember.repository;
-
-import com.ll.gramgram.instaMember.entity.InstaMemberSnapshot;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InstaMemberSnapshotRepository extends JpaRepository<InstaMemberSnapshot, Long> {
-}
