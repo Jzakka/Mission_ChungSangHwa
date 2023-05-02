68136ad935dc6d538f5f07b315838324aeddeea8
Jzakka <mouse4786@gmail.com>
2023-05-02 14:33:24 +0900

    test: 테스트 프로필 설정

--- src/test/java/com/ll/gramgram/boundedContext/notification/service/NotificationServiceTest.java
+++ src/test/java/com/ll/gramgram/boundedContext/notification/service/NotificationServiceTest.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -22,6 +23,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
+@ActiveProfiles("test")
 class NotificationServiceTest {
 
     @Autowired
