05f26e49eeb2a767e4596bfc6ecc768d1eb9ab59
Jzakka <mouse4786@gmail.com>
2023-05-02 14:37:08 +0900

    test: 테스트 프로필 설정

--- src/test/java/com/ll/gramgram/boundedContext/likeablePerson/repository/LikeablePersonRepositoryTest.java
+++ src/test/java/com/ll/gramgram/boundedContext/likeablePerson/repository/LikeablePersonRepositoryTest.java
@@ -5,6 +5,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,6 +13,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LikeablePersonRepositoryTest {
 
     @Autowired
